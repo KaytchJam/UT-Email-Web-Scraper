abaf8058ea392ce56bc121b2e35608d131dcdef7
KJam <kaytchjam@gmail.com>
2022-09-27 10:53:54 -0500

    testing out beautifulsoup HTMLParser

--- code/HTMLParser.py
+++ code/HTMLParser.py
@@ -1,2 +1,9 @@
-from bs4 import BeautifulSoup;
+from bs4 import BeautifulSoup
+from sqlalchemy import null;
 
+with open("documents/samplesite.html") as myFile:
+    soup = BeautifulSoup(myFile, features="html.parser")
+
+tag = soup.h
+print(tag)
+print(type(tag))
