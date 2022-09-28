9d23e4566cab49cfb9e09619c6c0c8ca07224e98
KJam <kaytchjam@gmail.com>
2022-09-28 12:33:48 -0500

    added the html source code of the cockrell school of engineering page and continuing to learn in HTMLParser.py

--- code/HTMLParser.py
+++ code/HTMLParser.py
@@ -1,9 +1,31 @@
 from bs4 import BeautifulSoup
-from sqlalchemy import null;
 
 with open("documents/samplesite.html") as myFile:
     soup = BeautifulSoup(myFile, features="html.parser")
 
-tag = soup.h
-print(tag)
-print(type(tag))
+# Printing Tag fields
+# tag = soup.h
+# print(tag)
+# print(type(tag))
+# print(tag.name)
+
+all_a = soup.findAll('a')
+print(all_a)
+
+counter = 1
+for tag in all_a:
+    print(str(counter) + ".")
+    print("tag name " + tag.name)
+    print("tag: ", end="")
+    print(tag)
+    print("tag attributes: ", end="")
+    print(tag.attrs)
+    print("tag contents: ", end="")
+    print(tag.contents)
+    print("tag children: ", end="")
+    print(tag.children)
+    print("tag descendants: ", end="")
+    print(tag.descendants)
+    print("tag string: ", end="")
+    print(tag.string)
+    counter += 1
