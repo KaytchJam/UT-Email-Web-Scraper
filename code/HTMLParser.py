fb098930fb686af673ffbffe4f53ade9947a04f4
KJam <kaytchjam@gmail.com>
2022-09-28 13:35:05 -0500

    added anthropology UT page source code for testcase

--- code/HTMLParser.py
+++ code/HTMLParser.py
@@ -2,6 +2,9 @@ import email
 from operator import contains
 from bs4 import BeautifulSoup
 
+header = "documents/"
+test_pages = tuple("cockrellpage.html", "anthrpologypage.html")
+
 with open("documents/cockrellpage.html", encoding='UTF-8') as myFile:
     soup = BeautifulSoup(myFile, features="html.parser",)
 
@@ -12,14 +15,12 @@ with open("documents/cockrellpage.html", encoding='UTF-8') as myFile:
 # print(tag.name)
 
 all_a = soup.findAll('a')
-#print(all_a)
-
 counter = 1
-
 email_list = list()
 
 for tag in all_a:
-    # loop through all 'a' tags, check if it has an email attribute
+    # loop through all 'a' tags, check if it has an email attribute'
+    #Cockrell & Natural Science method
     if "email" in tag.get_attribute_list('class'):
         print(str(counter) + ".")
         email_string = tag.string
