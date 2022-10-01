ea7bbec1263db523f2991b1c7a4712642625186c
KJam <kaytchjam@gmail.com>
2022-10-01 09:45:25 -0500

    Storing all sample sites in a tuple

--- code/HTMLParser.py
+++ code/HTMLParser.py
@@ -3,7 +3,7 @@ from operator import contains
 from bs4 import BeautifulSoup
 
 header = "documents/"
-test_pages = tuple("cockrellpage.html", "anthrpologypage.html")
+test_pages = tuple(["samplesite.html", "cockrellpage.html", "anthrpologypage.html"])
 
 with open("documents/cockrellpage.html", encoding='UTF-8') as myFile:
     soup = BeautifulSoup(myFile, features="html.parser",)
@@ -14,17 +14,23 @@ with open("documents/cockrellpage.html", encoding='UTF-8') as myFile:
 # print(type(tag))
 # print(tag.name)
 
-all_a = soup.findAll('a')
-counter = 1
-email_list = list()
+# loop through list of pages
+for page_index in len(test_pages):
+    # convert page into beautifulSoup object
+    with open(test_pages[page_index]) as myPage:
+        soup = BeautifulSoup(myFile, features="html.parser")
 
-for tag in all_a:
-    # loop through all 'a' tags, check if it has an email attribute'
-    #Cockrell & Natural Science method
-    if "email" in tag.get_attribute_list('class'):
-        print(str(counter) + ".")
-        email_string = tag.string
-        email_list.append(email_string)
-        print("Email: " + email_string + "\n")
-        counter += 1
+    all_a = soup.findAll('a')
+    counter = 1
+    email_list = list()
+
+    for tag in all_a:
+        # loop through all 'a' tags, check if it has an email attribute'
+        #Cockrell & Natural Science method
+        if "email" in tag.get_attribute_list('class'):
+            print(str(counter) + ".")
+            email_string = tag.string
+            email_list.append(email_string)
+            print("Email: " + email_string + "\n")
+            counter += 1
 
