377b1af040e47b749f98931c1424c5210ad31d91
KJam <kaytchjam@gmail.com>
2022-10-09 00:44:03 -0500

    changed comments to match python syntax

--- code/Driver.py
+++ code/Driver.py
@@ -4,6 +4,6 @@ from selenium import webdriver
 service = Service(executable_path="C:\Users\kaytc\edgedriver_win64\msedgedriver.exe")
 driver = webdriver.Edge(service=service)
 
-// navigating to web page (opening it)
-// driver.get("https://liberalarts.utexas.edu/academics/departments.html")
-// driver.back() to go back a page
+# navigating to web page (opening it)
+# driver.get("https://liberalarts.utexas.edu/academics/departments.html")
+# driver.back() to go back a page
