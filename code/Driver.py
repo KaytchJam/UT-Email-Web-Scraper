dfa18642165a7e86375fbca320a6d437878d5ad7
KJam <kaytchjam@gmail.com>
2022-10-09 00:41:06 -0500

    adding driver.py file for selenium web navigation

--- code/Driver.py
+++ code/Driver.py
@@ -3,3 +3,7 @@ from selenium import webdriver
 
 service = Service(executable_path="C:\Users\kaytc\edgedriver_win64\msedgedriver.exe")
 driver = webdriver.Edge(service=service)
+
+// navigating to web page (opening it)
+// driver.get("https://liberalarts.utexas.edu/academics/departments.html")
+// driver.back() to go back a page
