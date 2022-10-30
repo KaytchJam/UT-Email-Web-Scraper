79afb26bcdd4b72b7f7ce5bef2a7ac9d48a4ab9e
KJam <kaytchjam@gmail.com>
2022-10-30 14:15:22 -0500

    further updates on web driver

--- code/Driver.py
+++ code/Driver.py
@@ -1,3 +1,5 @@
+from logging import NullHandler
+from sre_parse import WHITESPACE
 from selenium.webdriver.edge.service import Service
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,9 +9,16 @@ from selenium.webdriver.common.by import By
 from operator import contains
 from bs4 import BeautifulSoup
 
+from HTMLParser import Email_Parser
+from HTMLParser import Liberal_Arts_Parser
+
 DRIVER_PATH = r"C:\Users\kaytc\edgedriver_win64\msedgedriver.exe"
 service = Service(executable_path=DRIVER_PATH)
 driver = webdriver.Edge(service=service)
+
+# Next Page Parser
+# Liberal Arts Parser
+# Sub Page Parser
 
     # navigating to web page (opening it)
     # driver.get("https://liberalarts.utexas.edu/academics/departments.html")
@@ -25,46 +34,85 @@ driver = webdriver.Edge(service=service)
                 # go back and repeat with next office-item tag
 try:
     # in the liberal arts general directory
+    driver.implicitly_wait(2)
     driver.get("https://liberalarts.utexas.edu/academics/departments.html")
-    s = BeautifulSoup(driver.page_source, 'html.parser')
     department_list = driver.find_element(By.CLASS_NAME, "offices-list")
-    department_at = department_list.find_element(By.CLASS_NAME, "offices-item")
-    department_link = department_at.find_element(By.TAG_NAME, "a")
+    dpt_links = Liberal_Arts_Parser.get_department_links(driver.page_source)
 
-    # in the AADS department
-    driver.get(department_link.get_property('href'))
-    print("AT AADS PAGE")
-    get_nav = driver.find_element(By.ID, "main-nav")
-    nav_items = get_nav.find_elements(By.TAG_NAME, "a")
-    faculty = nav_items[1]
+    for dpt in dpt_links:
+        print(dpt)
+        header = "https://liberalarts.utexas.edu"
+        driver.get(header + dpt)
+        get_nav = driver.find_element(By.ID, "main-nav")
+        nav_items = get_nav.find_elements(By.TAG_NAME, "a")
 
-    # aads faculty page
-    driver.get(faculty.get_property("href"))
-    print("AT AADS FACULTY PAGE")
-    
-    delay = 5 #seconds 
-    try:
-        filler_element = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "fac-info")))
-    except TimeoutException:
-        print("Took too long")
+        faculty = Liberal_Arts_Parser.get_faculty_link(driver.page_source)
 
-    html = driver.find_element(By.TAG_NAME, "html")
-    outer_html = html.get_attribute("outerHTML")
-    s = BeautifulSoup(outer_html, features='html.parser')
-    all_tags = s.findAll('a')
+        # print("going to faculty")
+        driver.get(header + faculty)
+        delay = 9 # seconds
 
-    print("WE GET DOWN HERE?")
-    #print(all_tags)
-    for tag in all_tags:
-        at_list = tag.get_attribute_list("href")
-        if at_list[0] is None:
+         # print("waiting")
+        try:
+            filler_element = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "fac-info")))
+        except TimeoutException:
+            print("Took too long")
+        
+        html = driver.find_element(By.TAG_NAME, "html")
+        outer_html = html.get_attribute("outerHTML")
+        s = BeautifulSoup(outer_html, features='html.parser')
+        print("writing to file")
+        all_tags = BeautifulSoup(outer_html, features='html.parser').findAll('a')
+
+        # print("EMAIL SET: ")
+        try:
+            with open("email_files" + dpt + ".txt", 'w') as email_file:
+                EP = Email_Parser("mailto", email_file)
+                EP.email_stream(EP.write_to_file, all_tags)
+        except FileExistsError:
             continue
-        else:
-            ref_string = at_list[0]
-            if len(ref_string) > 6 and ref_string[0:6] == "mailto":
-                print(ref_string[7:len(ref_string)])
+
+
+
+
+    # department_at = department_list.find_elements(By.CLASS_NAME, "offices-item")
+    # department_link = department_at[0].find_element(By.TAG_NAME, "a")
+
+    # # in the AADS department
+    # driver.get(department_link.get_property('href'))
+    # print("AT AADS PAGE")
+    # get_nav = driver.find_element(By.ID, "main-nav")
+    # nav_items = get_nav.find_elements(By.TAG_NAME, "a")
+    # faculty = nav_items[1]
+
+    # # aads faculty page
+    # driver.get(faculty.get_property("href"))
+    # print("AT AADS FACULTY PAGE")
+    
+    # delay = 5 #seconds 
+    # try:
+    #     filler_element = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "fac-info")))
+    # except TimeoutException:
+    #     print("Took too long")
+
+    # html = driver.find_element(By.TAG_NAME, "html")
+    # outer_html = html.get_attribute("outerHTML")
+    # s = BeautifulSoup(outer_html, features='html.parser')
+    # all_tags = s.findAll('a')
+
+    # print("WE GET DOWN HERE?")
+    # #print(all_tags)
+    # for tag in all_tags:
+    #     at_list = tag.get_attribute_list("href")
+    #     if at_list[0] is None:
+    #         continue
+    #     else:
+    #         ref_string = at_list[0]
+    #         if len(ref_string) > 6 and ref_string[0:6] == "mailto":
+    #             print(ref_string[7:len(ref_string)])
             
-except:
+except NotImplementedError:
+    print("Problem?")
     driver.quit()
     counter = 1
 
--- code/HTMLParser.py
+++ code/HTMLParser.py
@@ -1,67 +1,130 @@
+from io import TextIOWrapper
 from operator import contains
 from bs4 import BeautifulSoup
+from selenium.webdriver.edge.service import Service
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-# general test file header
-header = "documents/"
-MAIL_IDENTIFIER = "mailto"
+class Email_Parser:
+    DIRECTORY_HEADER = "email_files"
+    MAIL_IDENTIFIER = "mailto"
+    DEST_FILE = None
 
-# files we'll be using for testing
-SCHOOL_FACULTY_HTML_FILES = tuple(["naturalscipage.html", "cockrellpage.html", "anthropologypage.html", "jacksongeosciencepage.html", "fineartspage.html", "lawschoolpage.html",
-                                    "nursingschoolpage.html", "stevehickssocialworkpage.html"])
-EMAIL_FILE_NAMES = tuple(["Natural_Sci_Emails", "Cockrell_Engineering_Emails", "Anthropology_Emails", "Jackson_Geoscience_Emails", "Fine_Arts_Emails", "Law_School_Emails", 
-                                    "Nursing_School_Emails", "Steve_Hicks_SocialWork_Emails"])
+    # constructor 
+    def __init__(self, MAIL_STRING_IDENTIFIER = "mailto", DESTINATION_FILE = None):
+        self.MAIL_IDENTIFIER = MAIL_STRING_IDENTIFIER
+        self.set_destination_file(DESTINATION_FILE)
+    
+    # change the specified destination file
+    def set_destination_file(self, DESTINATION_FILE = None):
+        if isinstance(DESTINATION_FILE, TextIOWrapper) == False:
+            print("DESTINATION_FILE IS NOT A PROPER FILE and will be set to NONE")
+            self.DEST_FILE = None
+        else:
+            self.DEST_FILE = DESTINATION_FILE
 
-# headers for file creation
-DIRECTORY_HEADER = "email_files/"
-FILE_TYPE = ".txt"
+    # write to the file that you set
+    def write_to_file(self, some_string):
+        if self.DEST_FILE is None:
+            print("Could not write to file, as destination file is None. Please set destination file.")
+
+        self.DEST_FILE.write(some_string + "\n")
+    
+    # removes any leading whitespace in a given string
+    def clean_up_leading_whitespace(some_string = "   test"):
+        if some_string is None:
+            return None
+        
+        WHITESPACE = " "
+        while some_string[0] == WHITESPACE:
+            some_string = some_string[1:len(some_string)]
+        return some_string
+    
+    # from a given list of Beautiful Soup tags, return a stream of strings of emails contained in the tags and perform "operation" on them
+    def email_stream(self, operation = write_to_file, tag_list = None):
+        if tag_list is None:
+            print("tag list is None")
+            return 
+        
+        for tag in tag_list:
+            attr_list = tag.get_attribute_list("href")
+            if attr_list[0] is None:
+                continue
+            else:
+                current_href_string = attr_list[0]
+                if len(current_href_string) > 6 and current_href_string[0:6] == "mailto":
+                    email_string = current_href_string[7:len(current_href_string)]
+                    # print(email_string)
+                    operation(email_string)
+
+# helps parse the stuff on the liberal arts page
+class Liberal_Arts_Parser:
+
+    # return a list of links to each department page
+    def get_department_links(page_HTML):
+
+        soup = BeautifulSoup(page_HTML, features="html.parser")
+        o_list = soup.findAll('section', class_="offices-list") # the offices list element
+        o_list = o_list[0].findAll('div', class_="offices-item") # list of office items
+
+        department_links = list()
+        for o_item in o_list:
+            item_link = o_item.a
+            department_links.append(item_link['href'])
+        return department_links
+
+    # return the link that takes us to the faculty page for a given department
+    def get_faculty_link(page_HTML):
+        print("here")
+        soup = BeautifulSoup(page_HTML, features="html.parser")
+        nav_list = soup.findAll('nav', class_="subnav")
+        print(nav_list)
+        nav_list = nav_list[0].findAll('a')
+        print(nav_list)
+        print("nav loop")
+        for nav_link in nav_list:
+            print(nav_link['href'])
+            if "faculty" in nav_link['href']:
+                return nav_link['href']
+        
+        return None
 
 
-# TODO
-# listing which school/college each set of emails is from
-# get the web scraper to work
-# figure out the upload process (get all the emails onto a document or spreadsheet)
+    def _failed_preconditions(driver, d_list_or_link):
+        if d_list_or_link is None:
+            print("department list/link is none")
+            return True
+        elif driver is None:
+            print("driver is none")
+            return True
+        return False
 
-# Selenium Driver is needed for the following faculty pages:
-# School of Education
-# Mccombs School of Business 
-# School of Pharmacy (Austin) https://pharmacy.utexas.edu/people/austin
-# Dell Medical School https://intranet.dellmed.utexas.edu/public/committees-and-councils
-# LBJ School of Public Affairs https://lbj.utexas.edu/faculty-lbj-school-public-affairs
+    # navigate to a liberal art department's faculty page from a given department link
+    def get_to_faculty_page_outerHTML(self, driver, department_link):
+        if self._failed_preconditions(driver, department_link):
+            return None
 
-# ACTUAL HTML PARSING STUFF
+        driver.get(department_link.get_property('href'))
+        get_nav = driver.find_element(By.ID, "main-nav")
+        nav_items = get_nav.find_elements(By.TAG_NAME, "a")
+        faculty_nav_item = nav_items[1]
+        driver.get(faculty_nav_item.get_property("href"))
+        delay = 5 #seconds 
+        try:
+            filler_element = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "fac-info")))
+        except TimeoutException:
+            print("Took too long")
 
-# loop through list of pages
-for page_index in range(len(SCHOOL_FACULTY_HTML_FILES)):
-    # convert page into beautifulSoup object
-    with open("documents/" + SCHOOL_FACULTY_HTML_FILES[page_index], encoding="utf-8") as myPage:
-        soup = BeautifulSoup(myPage, features="html.parser")
+        html = driver.find_element(By.TAG_NAME, "html")
+        return html.get_attribute("outerHTML")
 
-    all_link_tags = soup.findAll('a') # get all link tags
-    STORAGE_FILE = DIRECTORY_HEADER + EMAIL_FILE_NAMES[page_index] + FILE_TYPE
+        
+            
 
-    # create a new email file
-    try:
-        with open(STORAGE_FILE, 'w') as email_file:
-            for tag in all_link_tags:
-                # loop through all 'a' tags, check if it has an email attribute'
-                #Cockrell & Natural Science method
-                attributeList = tag.get_attribute_list('href')
 
-                # does this element contain an href attribute?
-                if (attributeList[0] is None):
-                    continue
-                else:
-                    ref_string = attributeList[0]
-                    # do any hrefs in this element contain mail links?
-
-                    #print("PAGE PRINTED: " + str(len(ref_string) > 6))
-                    if len(ref_string) > 6 and ref_string[0:6] == MAIL_IDENTIFIER:
-                        an_email = ref_string[7:len(ref_string)]
-                        email_file.write(an_email + "\n")
-    except FileExistsError: 
-        # if the file already exists, simply skip it
-        print("File already exists: " + STORAGE_FILE)
-        continue
         
             
 
