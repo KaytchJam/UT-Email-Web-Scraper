72511a73dcebfa89f5ad5763b6680aafd78b4c79
KJam <kaytchjam@gmail.com>
2023-04-12 13:53:50 -0500

    prepping to convert txt files to a csv (temporary)

---
+++ code/TxtToCsv.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+newFrame = pd.DataFrame({0:[1, 2, 3],1:[4, 5, 6],2:[7,8,9]})
+print(newFrame)
+
+group_bitmap = 0b11110110011101111000110100101001110
+zero_mappings = tuple(
+    'School of Architecture', 
+    'Cockrell School of Engineering'
+    'Dell Medical School',
+    'College of Fine Arts')
