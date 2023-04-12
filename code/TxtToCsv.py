07df5ea2a8ce7d33d67289d189e566df0cac9211
KJam <kaytchjam@gmail.com>
2023-04-12 14:50:31 -0500

    putting work into the BitmapIterator class

--- code/TxtToCsv.py
+++ code/TxtToCsv.py
@@ -3,9 +3,47 @@ import pandas as pd
 newFrame = pd.DataFrame({0:[1, 2, 3],1:[4, 5, 6],2:[7,8,9]})
 print(newFrame)
 
-group_bitmap = 0b11110110011101111000110100101001110
-zero_mappings = tuple(
+group_bitmap = int(0b11110110011101111000110100101001110)
+print (group_bitmap)
+index = 34
+ZERO_MAPPINGS = tuple([
     'School of Architecture', 
     'Cockrell School of Engineering'
     'Dell Medical School',
-    'College of Fine Arts')
+    'College of Fine Arts',
+    'School of Information',
+    'Jackson School of Geosciences',
+    'LBJ School of Public Affairs',
+    'Mccombs School of Business',
+    'Moody School of Communication',
+    'College of Natural Sciences',
+    'College of Pharmacy',
+    'School of Law',
+    'School of Nursing',
+    'Steve Hicks School of Social Work'
+])
+
+LIBERAL_ARTS = 'College of Liberal Arts'
+ONE_MAPPINGS = tuple([
+    'African and African Disapora Studies',
+    'Air and Space Force Science',
+    'American Studies',
+    'Anthropology',
+    'Asian Studies',
+    'Classics',
+    'Economics',
+    'English',
+    'French and Italian',
+    'Geography and the Environment',
+    'Germanic Studies',
+    'Government',
+    'History',
+    'Linguistics',
+    'Mexican American and Latina/o Studies',
+    'Middle Eastern Studies',
+    'Naval Science',
+    'Rhetoric and Writing',
+    'Slavic and Eurasian Studies',
+    'Sociology',
+    'Spanish and Portuguese'
+])
